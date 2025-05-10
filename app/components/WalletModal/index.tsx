89a9f47422be6057b8b00b0998740f90f4ac4c9e
phamngocmy177 <phamngocmy177@gmail.com>
2025-05-10 20:19:13 +0700

    fix: show wallet logo on the modal

--- app/components/WalletModal/index.tsx
+++ app/components/WalletModal/index.tsx
@@ -9,77 +9,85 @@ import { TronLinkAdapterName } from '@tronweb3/tronwallet-adapters';
 import useWalletStore from '@/app/stores/wallet-store';
 
 interface WalletModalProps {
-  isOpen: boolean;
-  onClose: () => void;
+    isOpen: boolean;
+    onClose: () => void;
 }
 
 const WalletModal: React.FC<WalletModalProps> = ({ isOpen, onClose }) => {
-  if (!isOpen) return null;
-  const { connectAsync } = useConnect();
-  const { wallet: tronWallet, disconnect, select, connect: tronConnect } = useWallet();
-  const connectors = useWalletList();
-  const { connect , data} = useAppKitWallet();
-  const { connectWallet } = useWalletStore();
-  const handleConnect = async (connector: any) => {
-    connector.connector.disconnect()
-    if(connector.type === "tron") {
-        select(TronLinkAdapterName)
-        await tronConnect();
-        connectWallet({
-            id: connector.id,
-            address: tronWallet?.adapter.address || '',
-            network: 'tron',
-            connector: connector.connector
-        })
-        return;
-    }
-    if(connector.type === "evm") {
-        // await connect('metamask');
-        const connectedAccount = await connectAsync({ connector: connector.connector });
-        if(connectedAccount) {
+    if (!isOpen) return null;
+    const { connectAsync } = useConnect();
+    const { wallet: tronWallet, disconnect, select, connect: tronConnect } = useWallet();
+    const connectors = useWalletList();
+    const { connect, data } = useAppKitWallet();
+    const { connectWallet } = useWalletStore();
+    const handleConnect = async (connector: any) => {
+        connector.connector.disconnect()
+        if (connector.type === "tron") {
+            select(TronLinkAdapterName)
+            await tronConnect();
             connectWallet({
                 id: connector.id,
-                address: connectedAccount?.accounts?.[0] || '',
-                network: connectedAccount?.chainId || '',
+                address: tronWallet?.adapter.address || '',
+                network: 'tron',
                 connector: connector.connector
             })
+            return;
         }
-        return;
+        if (connector.type === "evm") {
+            // await connect('metamask');
+            const connectedAccount = await connectAsync({ connector: connector.connector });
+            if (connectedAccount) {
+                connectWallet({
+                    id: connector.id,
+                    address: connectedAccount?.accounts?.[0] || '',
+                    network: connectedAccount?.chainId || '',
+                    connector: connector.connector
+                })
+            }
+            return;
+        }
     }
-  }
-  console.log(data);
-  return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center">
-      {/* Backdrop */}
-      <div 
-        className="fixed inset-0 bg-black/50"
-        onClick={onClose}
-      />
-      
-      {/* Modal */}
-      <div className="relative bg-white/10 rounded-lg w-full max-w-md mx-4 p-6">
-        {/* Header */}
-        <div className="flex justify-between items-center mb-6">
-          <h2 className="text-xl font-semibold">Connect Wallet</h2>
-          <button 
-            onClick={onClose}
-            className="text-gray-500 hover:text-gray-700"
-          >
-            ✕
-          </button>
-        </div>
 
-        {/* Wallet Options */}
-        <div className='flex flex-col gap-4 justify-start items-center'>
-        {connectors.map((connector) => (
-          <button key={connector.id} onClick={() => handleConnect(connector)}>
-            {connector.name}
-          </button>
-        ))}
+    return (
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+            {/* Backdrop */}
+            <div
+                className="fixed inset-0 bg-black/50"
+                onClick={onClose}
+            />
+
+            {/* Modal */}
+            <div className="relative bg-white/10 rounded-lg w-full max-w-md mx-4 p-6">
+                {/* Header */}
+                <div className="flex justify-between items-center mb-6">
+                    <h2 className="text-xl font-semibold">Connect Wallet</h2>
+                    <button
+                        onClick={onClose}
+                        className="text-gray-500 hover:text-gray-700"
+                    >
+                        ✕
+                    </button>
+                </div>
+
+                {/* Wallet Options */}
+                <div className='flex flex-col gap-4 justify-start items-center'>
+                    {connectors.map((connector) => (
+                        <button key={connector.id} onClick={() => handleConnect(connector)}
+                            className='flex flex-row gap-2 justify-between items-center w-full'
+                        >
+                            <div className='flex flex-row gap-2 justify-start items-center'>
+                            {connector.icon ?
+                                <img src={connector.icon} alt={connector.name} className='w-10 h-10' />
+                                : null}
+                            <span className='text-sm'>{connector.type}</span>
+                            </div>
+                            <span className='text-sm'>{connector.name}</span>
+                        </button>
+                    ))}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default WalletModal;
